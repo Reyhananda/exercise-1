206025551a19c7a1e4337a783958a9c34f3c83a3
unknown <reyhanandasetiawan@gmail.com>
2020-08-04 15:13:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// for (let i = 0; i < 3; i++) {
+//     console.log(i)
+// }
+
+// var variabel = 'nilai'
+// let FirstName = 'Reyhananda'
+// console.log(variabel)
+// /* bangsat */
+// console.log/*kkk*/(FirstName)
+// console.log(100%2)
+// console.log(variabel + ' ' + FirstName)
+// console.log(`${variabel} ${FirstName}`)
+
+// console.log(contoh(5,5), 'b')
+
+let arr = ['rey', 19, 'UI']
+
+let obj = {
+    nama: 'rey',
+    umur: 19,
+    kampusIdaman: 'UI'
+}
+
+console.log(obj)
+
+
+/* 
+UNSTAGE
+
+
+STAGED
+
+
+PUSH
+
+*/
